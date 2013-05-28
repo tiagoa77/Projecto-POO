955be73079b51620b151e24e60acef51ce65e407
Marco Tiago Teixeira Leite Ribeiro <mtiagotlr@gmail.com>
2013-05-28 14:06:10 +0100

    registo completo

--- POO_Proje/Utilizador.java
+++ POO_Proje/Utilizador.java
@@ -11,6 +11,7 @@ public class Utilizador implements Serializable
     // instance variables - replace the example below with your own
     private String nome;
     private String morada;
+    private String pass;
     private List<Aposta> open_bet;
     private List<Aposta> bet_hist;
     private int conta_corr;
@@ -21,45 +22,50 @@ public class Utilizador implements Serializable
      */
     public Utilizador()
     {
-        nome="";
-        morada="";
-        open_bet=new ArrayList<Aposta>();
-        bet_hist=new ArrayList<Aposta>();
-        conta_corr=0;
+        this.nome="";
+        this.morada="";
+        this.pass="";
+        this.open_bet=new ArrayList<Aposta>();
+        this.bet_hist=new ArrayList<Aposta>();
+        this.conta_corr=0;
     }
 
-    public Utilizador(String nome_ut, String morada_ut, ArrayList<Aposta> open,
+    public Utilizador(String nome_ut, String morada_ut,String pass, ArrayList<Aposta> open,
     ArrayList<Aposta> bet_historico, int corr_count ) // arrays e conta corrente s‹o precisos como argumentos????
     {
-        nome=nome_ut;
-        morada=morada_ut;
-        open_bet=new ArrayList<Aposta>();
-        bet_hist=new ArrayList<Aposta>();
-        conta_corr=0;
+        this.nome=nome_ut;
+        this.morada=morada_ut;
+        this.pass=pass;
+        this.open_bet=new ArrayList<Aposta>();
+        this.bet_hist=new ArrayList<Aposta>();
+        this.conta_corr=0;
     }    
     public Utilizador(Utilizador uti)
     {
-        nome=uti.getNome();
-        morada=uti.getMorada();
-        open_bet=new ArrayList<Aposta>();
+        this.nome=uti.getNome();
+        this.morada=uti.getMorada();
+        this.pass=uti.getPass();
+        this.open_bet=new ArrayList<Aposta>();
         for(Aposta a : uti.getOpenBets()){
             open_bet.add(a.clone());
         }
-        bet_hist=new ArrayList<Aposta>();
+        this.bet_hist=new ArrayList<Aposta>();
         for(Aposta a : uti.getBetsHist()){
             bet_hist.add(a.clone());
         }
-        conta_corr=uti.getCC();
+        this.conta_corr=uti.getCC();
     }
     
     public String getNome(){ return this.nome; }
     public String getMorada(){ return this.morada; }
+    public String getPass(){return this.pass;}
     public List<Aposta> getOpenBets(){ return this.open_bet; }
     public List<Aposta> getBetsHist(){ return this.bet_hist;}
     public int getCC(){ return this.conta_corr; }
     
-    public void setNome(String nome_ut){ nome=nome_ut; }
-    public void setMorada(String morada_ut){ morada=morada_ut; }
+    public void setNome(String nome_ut){ this.nome=nome_ut; }
+    public void setMorada(String morada_ut){ this.morada=morada_ut; }
+    public void setPass(String pass){this.pass=pass;}
     public void setOpenBets(List<Aposta> ap){
         open_bet=ap;
         
@@ -80,9 +86,10 @@ public class Utilizador implements Serializable
     public void addBetsHist(Aposta ap){ bet_hist.add(ap); }
 
 
-    public void novoUtilizador(String nome,String morada){
+    public void novoUtilizador(String nome,String morada,String pass){
         this.setNome(nome);
         this.setMorada(morada);
+        this.setPass(pass);
         this.conta_corr = 0;
         this.open_bet = new ArrayList<Aposta>(); 
         this.bet_hist=new ArrayList<Aposta>();
